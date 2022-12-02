c654e260f9a6c03bd21477006e488601a38a164d
amanoizumi <a0918233246@gmail.com>
2022-12-02 21:24:45 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:amanoizumi/i-fitness.git main:gh-pages
+
+
+cd -
